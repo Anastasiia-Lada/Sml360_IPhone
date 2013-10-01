14727037eac9cb5efc10541acda2699b0bb88692
LadaAnastasiia <nastya_lada@mail.ru>
2013-10-01 21:46:24 +0300

    change permissionsList.publish_stream on login

--- app/view/ConnectPopUp.js
+++ app/view/ConnectPopUp.js
@@ -193,6 +193,8 @@ Ext.define('smiley360.view.ConnectPopUp', {
 				//loginToServer();
 			}
 		}, { scope: 'email, read_stream, publish_stream' });
+
+		smiley360.permissionsList.publish_stream = true;
 		//var deviceId = smiley360.services.getDeviceId();
 
 		//console.log('Login -> login to Facebook with deviceId: ', deviceId);
