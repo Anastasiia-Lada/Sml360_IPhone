24f2671cbe428d6fea28c5c008605b5217386520
LadaAnastasiia <nastya_lada@mail.ru>
2013-10-03 17:50:27 +0300

    added new "TermsofUse"

--- app/view/Main.js
+++ app/view/Main.js
@@ -41,7 +41,8 @@ Ext.define('smiley360.view.Main', {
                 ui: 'plain',
                 align: 'right',
                 cls: 'toolbar-btn-size toolbar-btn-menu-size',
-                width: '3em',
+                width: '4em',
+                padding: '0em 0em 0em 1em',
                 iconCls: 'menu-btn',
                 iconMask: true,
                 listeners: {
