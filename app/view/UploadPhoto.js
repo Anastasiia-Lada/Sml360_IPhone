587fe603fdb2f96a3137f9c32e8c4d206cf7b203
aGarik <agarik.ua@gmail.com>
2013-10-12 14:54:44 +0300

    validation button fix

--- app/view/UploadPhoto.js
+++ app/view/UploadPhoto.js
@@ -79,6 +79,7 @@ Ext.define('smiley360.view.UploadPhoto', {
                                         {
                                             xtype: 'fileupload',
                                             autoUpload: true,
+                                            name: 'xBrowsePhotoButton',
                                             cls: 'popup-photo-button has-shadow',
                                             states:
                                             {
@@ -95,7 +96,6 @@ Ext.define('smiley360.view.UploadPhoto', {
                                                 this.hide();
                                                 photoAdded = true;
                                                 this.up('#xView').config.imageID = response.imageID;
-                                                this.up('#xView').down('#xShareButton').enable();
                                             },
                                             failureHandler: function (response)
                                             {
@@ -253,60 +253,7 @@ Ext.define('smiley360.view.UploadPhoto', {
                 this.destroy();
             },
             painted: function ()
-            {/*
-                uploader = new plupload.Uploader({
-                    runtimes: 'html5,flash,silverlight,html4',
-                    browse_button: 'pickfiles', // you can pass in id...
-                    container: document.getElementById('container'), // ... or DOM Element itself
-                    url: 'upload.php',
-                    flash_swf_url: '../js/Moxie.swf',
-                    silverlight_xap_url: '../js/Moxie.xap',
-                    resize: { width: 100, height: 100, quality: 90 },
-
-
-                    filters: {
-                        max_file_size: '10mb',
-                        mime_types: [
-                            { title: "Image files", extensions: "jpg,gif,png" },
-                            { title: "Zip files", extensions: "zip" }
-                        ]
-                    },
-
-                    init: {
-                        PostInit: function ()
-                        {
-                            document.getElementById('filelist').innerHTML = '';
-
-                            document.getElementById('uploadfiles').onclick = function ()
-                            {
-                                uploader.start();
-                                return false;
-                            };
-                        },
-
-                        FilesAdded: function (up, files)
-                        {
-                            files = ////chosen file
-                            plupload.each(files, function (file)
-                            {
-                                document.getElementById('filelist').innerHTML += '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
-                            });
-                        },
-
-                        UploadProgress: function (up, file)
-                        {
-                            document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
-                        },
-
-                        Error: function (up, err)
-                        {
-                            document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
-                        }
-                    }
-                });
-
-                uploader.init();
-                */
+            {
                 smiley360.failedShares = [];
                 photoAdded = false;
                 //var fileName = smiley360.services.guid();
@@ -331,7 +278,7 @@ Ext.define('smiley360.view.UploadPhoto', {
 
     onCheck: function ()
     {
-        this.down('#xBrowsePhotoButton').setCls('popup-photo-button has-shadow');
+        this.down('[name=xBrowsePhotoButton]').setCls('popup-photo-button has-shadow');
         this.down('#xFacebookCheckbox').setLabelCls('popup-checkbox-grey-label');
         this.down('#xTwitterCheckbox').setLabelCls('popup-checkbox-grey-label');
         if (Ext.getCmp('xView')) Ext.getCmp('xView').doValidation();
@@ -416,7 +363,8 @@ Ext.define('smiley360.view.UploadPhoto', {
             var msg = '';
             if (!photoAdded)
             {
-                this.down('#xBrowsePhotoButton').setCls('popup-photo-button-required has-shadow');
+                if (this.down('[name=xBrowsePhotoButton]'))
+                    this.down('[name=xBrowsePhotoButton]').setCls('popup-photo-button-required has-shadow');
                 msg += 'Please, select some photo. ';
             }
             if (!his.down('#xTwitterCheckbox').getChecked() && his.down('#xFacebookCheckbox').getChecked())
@@ -443,8 +391,8 @@ Ext.define('smiley360.view.UploadPhoto', {
     {
         if (this.down('#xPostText').getValue().length < this.down('#xCharacterMaximum').config.xMax && ((this.down('#xFacebookCheckbox').getChecked() == true) || (this.down('#xTwitterCheckbox').getChecked() == true)) && photoAdded)
         {
-            if (photoAdded)
-                this.down('#xBrowsePhotoButton').setCls('popup-photo-button has-shadow');
+            if (photoAdded && this.down('[name=xBrowsePhotoButton]'))
+                this.down('[name=xBrowsePhotoButton]').setCls('popup-photo-button has-shadow');
             // Ext.getCmp('xShareButton').setDisabled(false);
             return true;
         }
